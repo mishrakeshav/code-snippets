b4ae2662666d649c7e66cbdb397636c46ba14ab7
mishrakeshav <mishrakeshav2001@gmail.com>
2024-08-20 21:59:59 +0530

    Add Chess and Matrix Moves

--- matrix.py
+++ matrix.py
@@ -1,1 +1,5 @@
-dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+# moves up,down,left,right
+moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+#moves up,down,left,right,diagonally
+moves = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
